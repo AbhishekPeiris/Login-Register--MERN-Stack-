bc298529aa8bb90afb2c0b0f0ca96f0980fa0905
Abhishek Peiris <128351417+AbhishekPeiris@users.noreply.github.com>
2024-01-16 09:36:48 +0530

    react coding

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const user = localStorage.getItem("token");
+
+    return (
+        <Router>
+            <Routes>
+                {user && <Route path="/" element={<Main />} />}
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Navigate replace to="/login" />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
